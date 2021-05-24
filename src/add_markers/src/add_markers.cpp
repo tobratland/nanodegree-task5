79ded84b0c7496fa34799c267db992ed616aac96
torbjørn bratland <tobratland@gmail.com>
2021-05-24 10:19:40 -0700

    refactored add_markers to oop

--- src/add_markers/src/add_markers.cpp
+++ src/add_markers/src/add_markers.cpp
@@ -1,107 +1,158 @@
-#include <ros/ros.h>
+#include "ros/ros.h"
 #include <visualization_msgs/Marker.h>
+#include "nav_msgs/Odometry.h"
 
-int main( int argc, char** argv )
+class AddMarkers
+{
+public:
+  AddMarkers(ros::NodeHandle *nodeHandle) : n_(*nodeHandle)
+  {
+    marker_pub_ = n_.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+    marker_pos_pub_ = n_.advertise<geometry_msgs::Point>("marker_position", 1);
+    ros::Subscriber sub = n_.subscribe("odom", 15, &AddMarkers::handleOdom, this);
+
+    initializePosiblePositions();
+
+    while (ros::ok)
+    {
+      if (!firstMarkerPlaced)
+      {
+        createMarker();
+        placeMarker();
+        firstMarkerPlaced = true;
+      }
+    }
+  }
+
+private:
+  ros::NodeHandle n_;
+  bool firstMarkerPlaced;
+  bool markerPickedUp;
+  float distanceTolerance;
+  bool markerPutDown;
+  std::vector<geometry_msgs::Point> posibilities;
+  visualization_msgs::Marker marker;
+  ros::Publisher marker_pub_;
+  ros::Publisher marker_pos_pub_;
+  geometry_msgs::Point currentPoint;
+
+  void handleOdom(const nav_msgs::Odometry::ConstPtr &msg)
+  {
+    if (!firstMarkerPlaced)
+    {
+      return;
+    }
+
+    if (!markerPickedUp && !markerPutDown)
+    {
+      if (distance(marker.pose.position.x, marker.pose.position.y, marker.pose.position.z, msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z) <= distanceTolerance)
+      {
+        marker.action = visualization_msgs::Marker::DELETE;
+        setMarkerPositionRandom();
+        placeMarker();
+        markerPickedUp = true;
+      }
+    }
+    if (markerPickedUp && !markerPutDown)
+    {
+      if (distance(marker.pose.position.x, marker.pose.position.y, marker.pose.position.z, msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z) <= distanceTolerance) {
+        marker.action = visualization_msgs::Marker::ADD;
+        placeMarker();
+        markerPutDown = true;
+      }
+    }
+  }
+
+  float distance(float x1, float y1,
+                 float z1, float x2,
+                 float y2, float z2)
+  {
+    return std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2) + std::pow(z2 - z1, 2) * 1.0);
+  }
+
+  void placeMarker()
+  {
+    currentPoint.x = marker.pose.position.x;
+    currentPoint.y = marker.pose.position.y;
+    currentPoint.z = marker.pose.position.z;
+  
+    marker_pub_.publish(marker);
+    marker_pos_pub_.publish(currentPoint);
+  }
+
+  void createMarker()
+  {
+    makeMarker();
+    setMarkerPositionRandom();
+  }
+
+  void initializePosiblePositions()
+  {
+    distanceTolerance = 1.0;
+    geometry_msgs::Point pos1;
+    pos1.x = 2.2;
+    pos1.y = 2.3;
+    pos1.z = 1.0;
+    posibilities.push_back(pos1);
+
+    geometry_msgs::Point pos2;
+    pos1.x = 2.2;
+    pos1.y = 2.3;
+    pos1.z = 1.0;
+    posibilities.push_back(pos2);
+
+    geometry_msgs::Point pos3;
+    pos1.x = 2.2;
+    pos1.y = 2.3;
+    pos1.z = 1.0;
+    posibilities.push_back(pos3);
+  }
+
+  void setMarkerPositionRandom()
+  {
+    int i = rand() % posibilities.size();
+    marker.pose.position.x = posibilities[i].x;
+    marker.pose.position.x = posibilities[i].y;
+    marker.pose.position.x = posibilities[i].z;
+
+    if (!firstMarkerPlaced)
+    {
+      posibilities.erase(posibilities.begin() + i - 1);
+    }
+  }
+
+  void makeMarker()
+  {
+    marker.header.frame_id = "map";
+    marker.header.stamp = ros::Time::now();
+
+    marker.ns = "basic_shapes";
+    marker.id = 0;
+
+    marker.type = visualization_msgs::Marker::SPHERE;
+
+    marker.action = visualization_msgs::Marker::ADD;
+
+    marker.scale.x = 0.4;
+    marker.scale.y = 0.4;
+    marker.scale.z = 0.4;
+
+    marker.color.r = 0.0f;
+    marker.color.g = 1.0f;
+    marker.color.a = 1.0;
+
+    marker.lifetime = ros::Duration();
+  }
+};
+
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "add_markers");
   ros::NodeHandle n;
-  ros::Rate r(1);
-  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
 
-  // Set our initial shape type to be a cube
-  uint32_t shape = visualization_msgs::Marker::CUBE;
+  AddMarkers addMarkers(&n);
 
-  while (ros::ok())
-  {
-    visualization_msgs::Marker first_marker;
-    visualization_msgs::Marker second_marker;
-    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-    first_marker.header.frame_id = "map";
-    first_marker.header.stamp = ros::Time::now();
+  ros::spin();
 
-    second_marker.header.frame_id = "map"; 
-    second_marker.header.stamp = ros::Time::now();
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
-    first_marker.ns = "basic_shapes";
-    first_marker.id = 0;
-
-    second_marker.ns = "basic_shapes";
-    second_marker.id = 1;
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-    first_marker.type = shape;
-    second_marker.type = shape;
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    first_marker.action = visualization_msgs::Marker::ADD;
-    second_marker.action = visualization_msgs::Marker::ADD;
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    first_marker.pose.position.x = 0;
-    first_marker.pose.position.y = 0;
-    first_marker.pose.position.z = 0;
-    first_marker.pose.orientation.x = 0.0;
-    first_marker.pose.orientation.y = 0.0;
-    first_marker.pose.orientation.z = 0.0;
-    first_marker.pose.orientation.w = 1.0;
-
-    second_marker.pose.position.x = 3;
-    second_marker.pose.position.y = 3;
-    second_marker.pose.position.z = 2;
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    first_marker.scale.x = 0.25;
-    first_marker.scale.y = 0.25;
-    first_marker.scale.z = 0.25;
-
-    second_marker.scale.x = 0.25;
-    second_marker.scale.y = 0.25;
-    second_marker.scale.z = 0.25;
-    // Set the color -- be sure to set alpha to something non-zero!
-    first_marker.color.r = 0.0f;
-    first_marker.color.g = 1.0f;
-    first_marker.color.b = 0.0f;
-    first_marker.color.a = 1.0;
-
-
-    second_marker.color.r = 1.0f;
-    second_marker.color.g = 0.5f;
-    second_marker.color.b = 0.5f;
-    second_marker.color.a = 1.0;
-
-
-    first_marker.lifetime = ros::Duration(5.0);
-    second_marker.lifetime = ros::Duration(5.0);
-    // Publish the marker
-    while (marker_pub.getNumSubscribers() < 1)
-    {
-      if (!ros::ok())
-      {
-        return 0;
-      }
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      sleep(1);
-    }
-    ROS_INFO_ONCE("adding first marker");
-    marker_pub.publish(first_marker);
-    sleep(10);
-    ROS_INFO_ONCE("adding second marker");
-    marker_pub.publish(second_marker);
-
-    // Cycle between different shapes
-    switch (shape)
-    {
-    case visualization_msgs::Marker::CUBE:
-      shape = visualization_msgs::Marker::SPHERE;
-      break;
-    case visualization_msgs::Marker::SPHERE:
-      shape = visualization_msgs::Marker::ARROW;
-      break;
-    case visualization_msgs::Marker::ARROW:
-      shape = visualization_msgs::Marker::CYLINDER;
-      break;
-    case visualization_msgs::Marker::CYLINDER:
-      shape = visualization_msgs::Marker::CUBE;
-      break;
-    }
-
-    r.sleep();
-  }
+  return 0;
 }
